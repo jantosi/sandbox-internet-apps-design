38a36f93bcbf7adbc3f321ed4e643ac2d9560ee3
Jakub Antosik <j.antosik@oberthur.com>
2015-10-10 12:24:18 +0200

    Bare Spring Boot, but it's running!

---
+++ src/test/java/pl/lodz/p/ftims/pai/PaiProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.ftims.pai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PaiProjectApplication.class)
+public class PaiProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
