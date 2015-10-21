2e8d9cee992827b7c0892c2a78a43f4b65147470
Andrzej Lisowski <andrzej.lisowski1@gmail.com>
2015-10-22 00:05:47 +0200

    Create test REST services #3

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into DELIVERY_MAN (id, name, surname, email) values (1, 'Jan', 'Nowak', 'jan.nowak@gmail.com');
+insert into DELIVERY_MAN (id, name, surname, email) values (2, 'Tomasz', 'Kowalski', 'tomaszekk@wp.pl');
+insert into DELIVERY_MAN (id, name, surname, email) values (3, 'Barbara', 'Buk', 'barbara12@onet.pl');
