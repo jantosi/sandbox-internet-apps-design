4a3f3f7e13511aeb16990796315eb479131e3925
Jakub Antosik <j.antosik@oberthur.com>
2016-02-05 21:29:21 +0100

    Department<-Headquarters push notification sync trigger

---
+++ centrala/src/main/java/pl/lodz/p/ftims/pai/service/model/DepartmentSyncRequest.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.ftims.pai.service.model;
+
+/**
+ * Created by antosikj (Jakub Antosik) on 05/02/16.
+ */
+public class DepartmentSyncRequest {
+    public String dummy;
+
+    public String getDummy() {
+        return dummy;
+    }
+
+    public void setDummy(String dummy) {
+        this.dummy = dummy;
+    }
+}
